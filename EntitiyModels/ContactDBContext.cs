25959bfdfb9235adfb36ea1d20a739bb6eeadb1c
Zahir Iqbal <zahiriqbal@users.noreply.github.com>
2020-05-03 02:22:38 +0100

    Update ContactDBContext.cs

--- EntitiyModels/ContactDBContext.cs
+++ EntitiyModels/ContactDBContext.cs
@@ -22,7 +22,7 @@ namespace AspNetCoreReactRedux.EntitiyModels
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=VOADEVLT009\\SQLEXPRESS;Database=ContactDB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=ContactDB;Trusted_Connection=True;");
             }
         }
 
